8242975842d3966dbe514e4a2d56751b82635d66
Jongyeol Yang <yangjy0113@gmail.com>
2016-01-24 21:22:50 +0900

    example to use summarize

---
+++ data-cleaning/ch03/summarize1.R
@@ -0,0 +1,6 @@
+by_package <- group_by(cran, package)
+pack_sum <- summarize(by_package,
+                      count = n(),
+                      unique = n_distinct(ip_id),
+                      countries = n_distinct(country),
+                      avg_bytes = mean(size))
