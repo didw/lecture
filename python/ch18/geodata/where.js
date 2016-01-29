1a294bcedff478ebdf37ab57909de14dfa9601fa
Jongyeol Yang <yangjy0113@gmail.com>
2016-01-30 00:10:48 +0900

    assignment

--- python/ch18/geodata/where.js
+++ python/ch18/geodata/where.js
@@ -1,14 +1,8 @@
 myData = [
-[42.340075,-71.0895367, 'Northeastern, Boston, MA 02115, USA'],
-[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA'],
-[32.778949,35.019648, 'Technion/ Sports Building, Haifa'],
-[18.4574518,73.8837999, 'Vishwakarma Institutes Play Ground, Yashodhan Society, Kapil Nagar, Kondhwa Budrukh, Vishwakarma, Maharashtra 411048, India'],
-[33.1561058,131.826132, 'Japan, 〒875-0002 Ōita-ken, Usuki-shi, Shitanoe, 1232−2 ＵＭＤ'],
-[42.4036847,-71.120482, 'South Hall Tufts University, 30 Lower Campus Rd, Somerville, MA 02144, USA'],
-[-37.914517,145.1303881, 'Monash College, Wellington Rd, Clayton VIC 3168, Australia'],
-[53.2948229,69.4047872, 'Kokshetau 020000, Kazakhstan'],
-[40.7127837,-74.0059413, 'New York, NY, USA'],
-[52.2869741,104.3050183, 'Irkutsk, Irkutsk Oblast, Russia'],
-[37.2439666,131.8597232, 'Dokdo-ri, Ulleung-eup, Ulleung-gun'],
-[28.6794861,115.9247409, 'Jeju Island Korean Cuisine （Shengtu South Road）, Donghu, Nanchang, Jiangxi, China, 330000']
+[37.566535,126.9779692, 'Seoul, South Korea'],
+[37.5172363,127.0473248, 'Gangnam-gu, Seoul, South Korea'],
+[37.2439666,131.8597232, 'Dokdo-ri, Ulleung-gun 799-800'],
+[33.4890113,126.4983023, 'Jeju-do, South Korea'],
+[35.1595454,126.8526012, 'Gwangju, South Korea'],
+[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA']
 ];
