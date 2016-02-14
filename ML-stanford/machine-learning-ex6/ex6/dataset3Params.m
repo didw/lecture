de2cb18b2409993421c2b2a94b2d3d63a1ed0dd2
Jongyeol Yang <yangjy0113@gmail.com>
2016-02-14 17:53:42 +0900

    assignment finished

--- ML-stanford/machine-learning-ex6/ex6/dataset3Params.m
+++ ML-stanford/machine-learning-ex6/ex6/dataset3Params.m
@@ -9,8 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
-
+sigma = 0.1;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -39,7 +38,7 @@ for i = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
   end
 end
 
-
+fprintf("final C = %d, sigma = %d\n", C, sigma);
 % =========================================================================
 
 end
